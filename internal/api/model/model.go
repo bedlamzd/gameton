18ed6bef79aaf8b03caec63939e14495b79642e4
bedlamzd <vcs@bedlamzd.dev>
2024-04-05 18:27:19 +0200

    Implement the rest of the API

--- internal/api/model/model.go
+++ internal/api/model/model.go
@@ -7,29 +7,61 @@ type (
 )
 
 type Planet struct {
-	Name string
+	Name    string
 	Garbage GarbageMap
 }
 
 type Ship struct {
 	CapacityX int
 	CapacityY int
-	FuelUsed int
-	Garbage GarbageMap
-	Planet Planet
+	FuelUsed  int
+	Garbage   GarbageMap
+	Planet    Planet
 }
 
 type UniverseEdge struct {
 	From string
-	To string
+	To   string
 	Cost int
 }
 
 type Universe = []UniverseEdge
 
 type UniverseResponse struct {
-	Name string
-	Ship Ship
+	Name     string
+	Ship     Ship
 	Universe [][]any
 }
 
+type PlanetDiff struct {
+	From string
+	Fuel int
+	To   string
+}
+
+type TravelResponse struct {
+	FuelDiff      int
+	PlanetDiffs   []PlanetDiff
+	PlanetGarbage GarbageMap
+	ShipGarbage   GarbageMap
+}
+type CollectResponse struct {
+	Garbage GarbageMap
+	Leaved  []GarbageID
+}
+
+type RoundInfo struct {
+	StartAt     string
+	EndAt       string
+	IsCurrent   bool
+	Name        string
+	PlanetCount int
+}
+
+type RoundsResponse struct {
+	Rounds []RoundInfo
+}
+
+type ResetRoundResponse struct {
+	Success bool
+}
