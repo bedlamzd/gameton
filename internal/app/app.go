ef78dd968fcfa6632619e5380581d7ac7e82a60e
marazzmo <Ozzmosystem@gmail.com>
2024-04-06 00:46:38 +0700

    draw(?)

--- internal/app/app.go
+++ internal/app/app.go
@@ -32,15 +32,28 @@ func DrawShit() {
 			SetText(text)
 	}
 
+	for i := 0; i < abob.Ship.CapacityY; i++ {
+		for j := 0; j < abob.Ship.CapacityX; j++ {
+			main.AddItem(newPrimitive(""), i, j, 1, 1, 0, 0, false)
+		}
+	}
+
+	for id, shape := range abob.Ship.Garbage {
+		for x, array := range shape {
+			for _, y := range array {
+				main.AddItem(newPrimitive(id), x, y, 1, 1, 0, 0, false)
+			}
+		}
+	}
+
 	menu := tview.NewList().
 		AddItem("Capacity X", strconv.Itoa(abob.Ship.CapacityX), 1, nil).
 		AddItem("Capacity Y", strconv.Itoa(abob.Ship.CapacityY), 1, nil).
 		AddItem("Fuel Used", strconv.Itoa(abob.Ship.FuelUsed), 1, nil).
 		AddItem("Planet", abob.Ship.Planet.Name, 1, nil)
 
-	button := tview.NewButton("Update").SetSelectedFunc(func() { DrawShit() })
-	button.SetBorder(true).SetRect(0, 0, 5, 5)
-	sideBar := button
+	sideBar := tview.NewList().
+		AddItem("Update", "Just Update Situation", 'u', func() { DrawShit() })
 
 	grid.SetRows(1, 0).
 		SetColumns(30, 0, 30).
